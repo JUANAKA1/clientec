62f5fa843cf6bb9b74aa3198395a56e0aa91a4f3
JUANAKA1 <kmiloprieto73@gmail.com>
2025-02-01 19:36:07 -0500

    icon cart and user

--- src/components/Layout/Account/Account.js
+++ src/components/Layout/Account/Account.js
@@ -1,11 +1,43 @@
 import { Button, Icon, Label } from "semantic-ui-react";
-import { useRouter } from "net/router";
+import { useRouter } from "next/router";
+import { useAuth} from "@/hooks";
 import styles from "./Account.module.scss";
+import classNames from "classnames";
+
+// Todo: ....
+const total = 5;
 
 export function Account() {
+    const { user } = useAuth();
+    const router = useRouter();
+
+    const goToLogin = () => router.push("/join/sign-in");
+    const goToAccount = () => router.push("/account");
+
+    const goToCart = () => {
+        if (!user) goToLogin();
+            else router.push("/cart");
+    }
   return (
-    <div>
-     <span>Account....</span>
+    <div className={styles.account}>
+        <Button icon className={styles.cart}>
+            <Icon name="cart" onClick={goToCart} />
+            {total > 0 && <Label circular>{total}</Label>}
+        </Button>
+
+        <Button icon className={classNames({[styles.user]: user})}>
+            <Icon name="user outline" onClick={user ? goToAccount : goToLogin} />
+        </Button>
+        {/* {!user ? (
+            <Button icon>
+                <Icon name="user outline" onClick={goToLogin}/>
+            </Button>
+        ) : (
+            <Button icon className={styles.user}>
+                <Icon name="user outline" onClick={goToAccount}/>
+            </Button>
+        ) } */}
+
     </div>
   )
 }
