cfcb6adf36176c8b3d9fd70054e914283815cc80
JUANAKA1 <kmiloprieto73@gmail.com>
2025-02-09 13:08:39 -0500

    gamer platforms

---
+++ src/components/Shared/BarTrust/BarTrust.data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        icon: "truck",
+        title: "Envíos gratis",
+        description: "Entregamos a cualquier dirección en 3 días hábiles"
+    },
+    {
+        icon: "shield",
+        title: "Fiable y seguro",
+        description: "Más de 10.000 productos"
+    },
+    {
+        icon: "wechat",
+        title: "Soporte 24/7",
+        description: "Contáctanos en cualquier momento para responder a tus dudas"
+    }
+]
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,14 @@
 import { Container } from "semantic-ui-react";
 import { BasicLayout } from "@/layouts";
 import { Home } from "@/components/Home";
-import { Separator } from "@/components/Shared";
+import { BarTrust, Separator } from "@/components/Shared";
+
+const platformsId = {
+  playstation: 2,
+  xbox: 4, //id de platform en strapi no conside con el de la consola, hay sumar +1
+  nintendo: 8,
+  pc: 6,
+}
 
 export default function HomePage() {
   return (
@@ -17,6 +24,20 @@ export default function HomePage() {
         </Container>
 
         <Separator height={100} />
+
+        <BarTrust />
+
+        <Separator height={100} />
+
+        <Container>
+          <Home.LatestGames 
+          
+            title="PlayStation" 
+            limit={3}
+            platformId={platformsId.playstation}
+          />
+        </Container>
+
         <Separator height={100} />
 
       </BasicLayout>
