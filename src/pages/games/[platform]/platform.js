b9702b2308507680d0561739c23303b26f0b4549
JUANAKA1 <kmiloprieto73@gmail.com>
2025-02-10 13:40:59 -0500

    rendering platforms

--- src/pages/games/[platform]/platform.js
+++ src/pages/games/[platform]/platform.js
@@ -1,12 +1,32 @@
+import { GridGames, Separator } from "@/components/Shared";
+import { BasicLayout } from "@/layouts";
+import { size } from "lodash";
+import { Container } from "semantic-ui-react";
 
 export default function PlatformPage(props) {
-    // const { platform } = props;
-    console.log(props);
+    const { games, platform, pagination } = props;
+    const hasProducts = size(games) > 0;
     
   return (
-    <div>
-      <h2>Platform</h2>
-    </div>
+    <>
+      <BasicLayout relative>
+        <Container>
+          <Separator height={50} />
+
+          <h2>{platform.title}</h2>
+
+          {hasProducts ? (
+            <>
+              <GridGames games={games} />
+            </>
+          ):(
+            <p>No results</p>
+          )}
+          
+          <Separator height={100} />
+        </Container>
+      </BasicLayout>
+    </>
   )
 }
 
