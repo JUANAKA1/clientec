3f133649f75ce139c3e7485a68a99cc31c1333d2
JUANAKA1 <kmiloprieto73@gmail.com>
2025-02-27 18:02:40 -0500

    server

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-    SERVER_HOST: "http://localhost:1337",
-    API_URL: "http://localhost:1337/api",
+    SERVER_HOST: "http://serverecommerce-production-a590.up.railway.app",
+    API_URL: "http://serverecommerce-production-a590.up.railway.app/api",
     ENDPOINTS: {
         AUTH: {
             REGISTER: "auth/local/register",
